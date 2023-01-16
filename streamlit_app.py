eea371768f064954cfb61806dac9d659f1a53ff4
infohetvipatel <62741780+infohetvipatel@users.noreply.github.com>
2023-01-16 10:13:53 +0530

    Update streamlit_app.py
    Added one header
    Added three text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('New healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry')
+streamlit.text('Kale Spinach and Rocket Smoothie')
+streamlit.text('Hard boiled fire range egg')
